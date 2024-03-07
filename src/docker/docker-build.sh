78566b3c7f303a9a1491162046d56c47529caa03
Michael Quinn <153249112+michquinn@users.noreply.github.com>
2024-03-07 00:59:29 -0500

    Create hello world container

--- src/docker/docker-build.sh
+++ src/docker/docker-build.sh
@@ -1,17 +1,14 @@
 #!/bin/bash
 
 echo "Ensure we are running in the proper folder"
-if !([[ -d "./src/docker" ]] && [[ -f "./src/TODO:SomeFile" ]]); then 
+if !([[ -d "./src/docker" ]] && [[ -f "./src/app/main.py" ]]); then
     echo "This script must be run from the repository root folder"
     exit 1
 fi
 
 echo "Building Docker Image"
-docker build --file src/docker/Dockerfile --tag ghcr.io/agile-learning-institute/mentorhub-<<TODO:domaion>>-<<TODO:component>>:latest .
+docker build --rm --file src/docker/Dockerfile --tag ghcr.io/agile-learning-institute/mentorhub-topic-api:latest .
 if [ $? -ne 0 ]; then
     echo "Docker build failed"
     exit 1
 fi
-
-# Run the containers
-mh up TODO:package
