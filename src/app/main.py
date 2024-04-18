9988e8b580dbec1a35035d034f0425c5614e0bbc
Michael Quinn <153249112+michquinn@users.noreply.github.com>
2024-04-18 13:40:14 -0400

    Add missing endpoints to Flask

--- src/app/main.py
+++ src/app/main.py
@@ -6,8 +6,10 @@ def hello_world(**kwargs):
     return "Hello, World!"
 
 app.add_url_rule("/api/topic", methods=["GET","POST"], view_func=hello_world)
-app.add_url_rule("/api/topic/<id>", methods=["GET","PATCH"], view_func=hello_world)
-app.add_url_rule("/api/topic/<id>/resource", methods=["POST"], view_func=hello_world)
-app.add_url_rule("/api/topic/<id>/resource/<name>", methods=["PATCH","DELETE"], view_func=hello_world)
+app.add_url_rule("/api/topic/<topicid>", methods=["GET","PATCH"], view_func=hello_world)
+app.add_url_rule("/api/topic/<topicid>/resource", methods=["POST"], view_func=hello_world)
+app.add_url_rule("/api/topic/<topicid>/resource/<resourceid>", methods=["PATCH","DELETE"], view_func=hello_world)
+app.add_url_rule("/api/path", methods=["GET","POST"], view_func=hello_world)
+app.add_url_rule("/api/path/<pathid>", methods=["PATCH"], view_func=hello_world)
 app.add_url_rule("/api/health", methods=["GET"], view_func=hello_world)
 app.add_url_rule("/api/config", methods=["GET"], view_func=hello_world)
