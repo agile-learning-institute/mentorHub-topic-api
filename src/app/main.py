e0a904f1a0f60fce212178d7f6026187257ea9c9
Michael Quinn <153249112+michquinn@users.noreply.github.com>
2024-04-24 00:13:22 -0400

    Fix incorrect and missing endpoints

--- src/app/main.py
+++ src/app/main.py
@@ -14,12 +14,13 @@ def response(object):
         return json.dumps(data[object])
 
 
-@app.route("/api/topic", methods=["GET","POST"])
+@app.route("/api/topic", methods=["GET"])
 def return_topic_identifiers():
     output = response('topic_identifiers')
 
     return output
 
+@app.route("/api/topic", methods=["POST"])
 @app.route("/api/topic/<topicid>", methods=["GET","PATCH"])
 @app.route("/api/topic/<topicid>/resource", methods=["POST"])
 @app.route("/api/topic/<topicid>/resource/<resourceid>", methods=["PATCH","DELETE"])
@@ -28,13 +29,14 @@ def return_topic(**kwargs):
 
     return output
 
-@app.route("/api/path", methods=["GET","POST"])
+@app.route("/api/path", methods=["GET"])
 def return_path_identifiers():
     output = response('path_identifiers')
 
     return output
 
-@app.route("/api/path/<pathid>", methods=["PATCH"])
+@app.route("/api/path", methods=["POST"])
+@app.route("/api/path/<pathid>", methods=["GET","PATCH"])
 def return_path(**kwargs):
     output = response('path')
 
