39b5176497b5b47f70397af0c732772ca2f63000
Michael Quinn <153249112+michquinn@users.noreply.github.com>
2024-09-01 10:45:02 -0400

    Consolidate Content-Type logic

--- src/app/main.py
+++ src/app/main.py
@@ -1,4 +1,4 @@
-from flask import Flask, request
+from flask import Flask, request, Blueprint
 from database import *
 import os
 import pymongo.errors
@@ -7,53 +7,43 @@ app = Flask(__name__)
 
 db = make_database_connection(os.getenv('CONNECTION_STRING', 'mongodb://root:example@mentorhub-mongodb:27017/?tls=false&directConnection=true'))
 
+topic_routes = Blueprint('topic_routes', __name__)
+
 def hello_world(**kwargs):
     return "Hello, World!"
+
+@topic_routes.after_request
+def set_content_type(response):
+    if response.status_code == 200:
+        response.content_type = 'application/json'
+    return response
 
 @app.errorhandler(pymongo.errors.WriteError)
 def write_error_handler(error):
     app.logger.error(error)
     return "Write Error Occured", 500
 
-@app.get("/api/topic")
+@topic_routes.get("/api/topic")
 def get_topics():
-    response = app.make_response(find_topics(db))
+    return app.make_response(find_topics(db))
 
-    response.content_type = 'application/json'
-
-    return response
-
-@app.post("/api/topic")
+@topic_routes.post("/api/topic")
 def post_topic():
-    response = app.make_response(insert_topic(db, request.data))
+    return app.make_response(insert_topic(db, request.data))
 
-    response.content_type = 'application/json'
-
-    return response
-
-@app.get("/api/topic/<topicid>")
+@topic_routes.get("/api/topic/<topicid>")
 def get_topic_by_id(topicid):
-    response = app.make_response(find_topic_by_id(db, topicid))
+    return app.make_response(find_topic_by_id(db, topicid))
 
-    response.content_type = 'application/json'
-
-    return response
-
-@app.get("/api/path")
+@topic_routes.get("/api/path")
 def get_paths():
-    response = app.make_response(find_paths(db))
+    return app.make_response(find_paths(db))
 
-    response.content_type = 'application/json'
-
-    return response
-
-@app.get("/api/path/<pathid>")
+@topic_routes.get("/api/path/<pathid>")
 def get_path_by_id(pathid):
-    response = app.make_response(find_path_by_id(db, pathid))
+    return app.make_response(find_path_by_id(db, pathid))
 
-    response.content_type = 'application/json'
-
-    return response
+app.register_blueprint(topic_routes)
 
 app.add_url_rule("/api/topic/<topicid>", methods=["PATCH"], view_func=hello_world)
 app.add_url_rule("/api/topic/<topicid>/resource", methods=["POST"], view_func=hello_world)
