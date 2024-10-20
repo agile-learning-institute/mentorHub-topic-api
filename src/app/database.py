61603e3295b0251a43968f3f8590bf1d0dd5de7e
Michael Quinn <153249112+michquinn@users.noreply.github.com>
2024-10-20 11:01:32 -0400

    Update Mongo topic query to reflect schema change

--- src/app/database.py
+++ src/app/database.py
@@ -11,30 +11,6 @@ def find_topic_by_id(db, topic_id):
     pipeline = [
             { "$match": { '_id': ObjectId(topic_id) } },
             {
-                "$lookup": {
-                    "from": 'resources',
-                    "localField": 'resources',
-                    "foreignField": '_id',
-                    "as": 'resources'
-                }
-            },
-            {
-                "$lookup": {
-                    "from": 'skills',
-                    "localField": 'skills',
-                    "foreignField": '_id',
-                    "as": 'skills'
-                }
-            },
-            {
-                "$lookup": {
-                    "from": 'skills',
-                    "localField": 'resources.skills',
-                    "foreignField": '_id',
-                    "as": 'resources_skills'
-                }
-            },
-            {
                 "$set": {
                     "id": {
                         "$toString": '$_id'
@@ -42,46 +18,8 @@ def find_topic_by_id(db, topic_id):
                 }
             },
             {
-                "$set": {
-                    "resources": {
-                        "$map": {
-                            "input": '$resources',
-                            "as": 'resource',
-                            "in": {
-                                "id": {
-                                    "$toString": '$$resource._id'
-                                },
-                                "name": '$$resource.name',
-                                "status": '$$resource.status',
-                                "description": '$$resource.description',
-                                "duration": '$$resource.duration',
-                                "link": '$$resource.link',
-                                "skills": {
-                                    "$filter": {
-                                        "input": '$resource_skills',
-                                        "as": 'skill',
-                                        "cond": {
-                                            "$in": [ '$$skill._id', '$$resource.skills' ]
-                                        }
-                                    }
-                                }
-                            }
-                        }
-                    }
-                }
-            },
-            {
-                "$addFields": {
-                    'resources.skills': '$resource_skills'
-                    }
-                },
-            {
                 "$project": {
                     '_id': 0,
-                    'lastSaved': 0,
-                    'resources_skills': 0,
-                    'skills.lastSaved': 0,
-                    'skills._id': 0
                 }
             }
     ]
